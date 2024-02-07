82f662216c98e3e712a9f3f2d35cc8af7bf7efb4
Jason Shipp <jason.s.shipp@gmail.com>
2024-02-07 13:34:05 -0500

    Add print statement to sane_wait_for to isolate timeout

--- discord/utils.py
+++ discord/utils.py
@@ -709,6 +709,7 @@ async def async_all(
 
 
 async def sane_wait_for(futures: Iterable[Awaitable[T]], *, timeout: Optional[float]) -> Set[asyncio.Task[T]]:
+    print(futures, timeout)
     ensured = [asyncio.ensure_future(fut) for fut in futures]
     done, pending = await asyncio.wait(ensured, timeout=timeout, return_when=asyncio.ALL_COMPLETED)
 
